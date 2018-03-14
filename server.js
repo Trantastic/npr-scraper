5b7c7d0d52ab13595f1a697f6d7eba41aa0ccde0
Yvette <yvettetran@gmail.com>
2018-03-13 22:03:57 -0700

    connected routes to handlebars & started route to display all articles in db

--- server.js
+++ server.js
@@ -4,24 +4,27 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
+var exphbs = require("express-handlebars");
 
 // Requiring everything in models folder
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/newsScrape"
-// 	, {
-//   useMongoClient: true
-// }
-);
+mongoose.connect("mongodb://localhost/newsScrape", 
+	{useMongoClient: true});
 
+
+var mainRoutes = require("./routes/main-routes.js")(app);
+
+// Route to scrape articles 
 app.get("/scrape", function(req, res){
 	request("https://www.npr.org/sections/world/", function(error, response, html){
 		var $ = cheerio.load(html);
@@ -31,7 +34,7 @@ app.get("/scrape", function(req, res){
 			var title = $(element).find("h2.title").find("a").text();
 			var link = $(element).find("h2.title").find("a").attr("href");
 			var summary = $(element).find("p.teaser").find("a").text();
-
+			// Prevents articles with null titles and links from being in db
 			if(title && link){
 				var result = {};
 
@@ -49,6 +52,17 @@ app.get("/scrape", function(req, res){
 			}
 		});
 	});
+});
+
+// Route to display all articles from db
+app.get("/articles", function(req, res){
+	db.Article.find({})
+		.then(function(dbArticle){
+			res.json(dbArticle);
+		})
+		.catch(function(error){
+			res.json(error);
+		});
 });
 
 app.listen(8080, function() {
